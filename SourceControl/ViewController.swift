11197572fa0362c91d53df80ebe8dbba1304050e
Emma <Emma@macbook.home>
2019-03-26 10:37:29 +0100

    Initial Commit

---
+++ SourceControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SourceControl
+//
+//  Created by Emma on 26/03/2019.
+//  Copyright © 2019 Emma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
